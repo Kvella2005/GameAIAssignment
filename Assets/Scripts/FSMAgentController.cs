7dec31a922ed911e31152d8cf51fc33ccb91000f
Kyle-Vella <kyle.vella.g52651@mcast.edu.mt>
2026-01-09 18:44:34 +0100

    Created guard's patrol functionality
    Patrol functionality works by setting next destination after the guard comes closer to the destination. The destination is selected randomly with use of random number generator.

--- Assets/Scripts/FSMAgentController.cs
+++ Assets/Scripts/FSMAgentController.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FSMAgentController : MonoBehaviour
 {
@@ -9,15 +12,67 @@ public class FSMAgentController : MonoBehaviour
         Search
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    State currentState;
+
+    Animator fsmAnimator;
+
+    //ai navigation
+    NavMeshAgent agent;
+
+    //guards destination
+    [SerializeField] private List<Vector3> goals;
+    
+    //guards movement
+    private Vector3 pos;
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float chaseSpeed = 5f;
+    [SerializeField] Transform target;
+
+    List<Transform> targets;
+    int currentIndex;
+    int previousIndex;
+
+    void Awake()
+    {
+        //get components before scene starts
+        fsmAnimator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        pos = transform.position;
+
+        GameObject[] goalGOs = GameObject.FindGameObjectsWithTag("guardGoal");
+
+        //to find each destination for the guard
+        foreach (var goalGO in goalGOs)
+        {
+            goals.Add(goalGO.transform.position);
+        }
+    }   
+
     void Start()
     {
-        
+        SetNextGoal();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Patrol()
+    {
+        if(agent.remainingDistance <= .2f)
+        {
+            SetNextGoal();
+            agent.destination = goals[currentIndex];
+        }
+    }
+
+    void SetNextGoal()
+    {
+        do
+        {
+            currentIndex = UnityEngine.Random.Range(0, goals.Count);
+        } while (currentIndex == previousIndex);
     }
 }
